0de5013fd1461df637ef3088f7c35465c277e316
rizwan-ahammed <rizwan@keyvalue.systems>
2025-05-06 14:17:22 +0530

    Update bash_and_python_hello_world.py

--- bash_and_python_hello_world.py
+++ bash_and_python_hello_world.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # Define a simple Python function to print "Hello World"
 def print_hello_world():
-    print("Hello World from Python d!")
+    print("Hello World from Python!")
 
 # Define the DAG
 with DAG(
@@ -19,13 +19,23 @@ with DAG(
     # Task 1: Run a bash command to print "Hello World"
     bash_task = BashOperator(
         task_id='bash_hello_world',
-        bash_command='echoo "Hello World from Bash!"'
+        bash_command='echo "Hello World from Bash!"',
+        executor_config={
+            "KubernetesExecutor": {
+                "image": "alpine:latest",  # or any image with `echo` shell
+            }
+        },
     )
 
     # Task 2: Run a Python function to print "Hello World"
     python_task = PythonOperator(
         task_id='python_hello_world',
-        python_callable=print_hello_world
+        python_callable=print_hello_world,
+        executor_config={
+            "KubernetesExecutor": {
+                "image": "python:3.9",  # ensure the image has necessary Python libs
+            }
+        },
     )
 
     # Set task dependencies
